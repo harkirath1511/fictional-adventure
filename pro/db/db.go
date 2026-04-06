17383098049c9048d5d9136c133dc6407e717048
Harkirat <singharkirath1511@gmail.com>
2026-04-06 16:33:44 +0530

    start!!

---
+++ pro/db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+const connectionString = "mongodb+srv://singharkirath1511_db_user:Harkirat@123@cluster.e6wscec.mongodb.net/?appName=Cluster"
+const dbName = "netflix"
+const colName = "watchlist"
+
+var Collection *mongo.Collection
+
+func init() {
+	clientOptions := options.Client().ApplyURI(connectionString)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal("Noooo!!! \n err : ", err)
+	}
+	fmt.Println("Mongodb connection success")
+
+	Collection = client.Database(dbName).Collection(colName)
+
+	fmt.Println("Collection instance is ready")
+
+}
